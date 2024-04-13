cf12e3cd287953c0f5ae82541b0c20b78f883d0b
David Arhin <davidarhin2005@gmail.com>
2024-04-13 13:16:06 +0000

    nextjs--cart

---
+++ utils/truncateText.ts
@@ -0,0 +1,9 @@
+import { IoRestaurantSharp } from "react-icons/io5"
+
+export const truncateText = 
+(str: string) =>{
+    if(str.length<25) return str
+
+
+      return  str.substring(0, 25) + "...";
+}
