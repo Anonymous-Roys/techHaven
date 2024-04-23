f07e6509a835f9feab77fad00298d46d89a0aba6
David Arhin <davidarhin2005@gmail.com>
2024-04-23 22:59:40 +0000

    listrating

--- app/product/[productId]/page.tsx
+++ app/product/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import { product } from "@/utils/product";
 import ProductDetails from "./ProductDetails";
 import Container from "@/app/components/Container";
+import ListRating from "./ListRating";
 
 interface IPrams{
     productId? : string
@@ -12,6 +13,15 @@ const Product = ({params} : {params:IPrams}) => {
     return ( <div className="p-8">
         <Container>
             <ProductDetails product={ product }/>
+            <div className="
+                flex
+                flex-col
+                mt-20
+                gap-4
+            ">
+                <div>Add Rating</div>
+                <ListRating product={product}/>
+            </div>
         </Container>
 
     </div> );
