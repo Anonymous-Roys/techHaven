221dca478dd684878b975f2691b92760fd57a6b8
Arhin David Kwabena <davidarhin2005@gmail.com>
2024-06-02 23:57:56 +0000

    importing providers

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
+import GoogleProvider from "next-auth/providers/google"
  
 const prisma = new PrismaClient()
  
