841673717ee01d543da7649caa0e0b3902643aed
Arhin David Kwabena <davidarhin2005@gmail.com>
2024-06-02 23:57:03 +0000

    google adaptor

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,6 +9,11 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     GoogleProvider({
          clientId: process.env.GOOGLE_CLIENT_ID,
+         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        //  authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
+        //  accessTokenUrl: 'https://www.googleapis.com/oauth2/v4/token',
+        //  profileUrl: 'https://www.googleapis.com/oauth2/v3/userinfo',
+        //  scope: ['profile', 'email'],
     })
   ],
 })
