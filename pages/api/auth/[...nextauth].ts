cb15032160bd9d3fba4a2877ee451d0bc5e7ce1a
Arhin David Kwabena <davidarhin2005@gmail.com>
2024-06-04 00:11:14 +0000

    prisma configuration

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
 import prisma from "@libs/prismadb" 
 
  
@@ -11,10 +12,21 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     GoogleProvider({
          clientId: process.env.GOOGLE_CLIENT_ID as string,
          clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+
+
         //  authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
         //  accessTokenUrl: 'https://www.googleapis.com/oauth2/v4/token',
         //  profileUrl: 'https://www.googleapis.com/oauth2/v3/userinfo',
         //  scope: ['profile', 'email'],
+    })
+    CredentialsProvider({
+        name: 'credentials',
+        credentials:{
+            email:{
+                label: 'Email',
+                type: 'text',
+            }
+        }
     })
   ],
 })
