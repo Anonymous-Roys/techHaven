cfe311d8f46214081e185d517540fb08444f7a6d
Arhin David Kwabena <davidarhin2005@gmail.com>
2024-06-14 02:43:35 +0000

    user

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -2,6 +2,7 @@ import Stripe from "stripe";
 import prisma from "@/libs/prismadb";
 import { NextResponse } from "next/server";
 import { CartProductType } from "@/app/product/[productId]/ProductDetails";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     apiVersion: "2022-11-15",
